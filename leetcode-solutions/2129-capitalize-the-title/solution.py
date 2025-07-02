b1d31c0707722c07ca5b6e1dcb65292afa6f5b04
Daniel Kebede <ethiomiracle2017@gmail.com>
2025-07-02 13:14:31 +0300

    Update solution.py

--- leetcode-solutions/2129-capitalize-the-title/solution.py
+++ leetcode-solutions/2129-capitalize-the-title/solution.py
@@ -7,3 +7,27 @@ class Solution:
             word.lower() if len(word) <= 2 else word.capitalize()
             for word in title.split()
         )
+# or 
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        words = title.title().split()
+        result = []
+        for word in words:
+            if len(word) <= 2:
+                result.append(word.lower())
+            else:
+                result.append(word)
+        return " ".join(result)
+# or 
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        words= title.split()
+        work = []
+        for num in words:
+            if len(num)<=2:
+                work.append(num.lower())
+            else:
+                work.append(num[0].upper() + num[1:].lower())
+        return " ".join(work)
+
+
