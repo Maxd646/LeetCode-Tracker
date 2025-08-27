c892ef44057e158b877e64d001e160648eb7333a
Daniel <ethiomiracle2017@gmail.com>
2025-08-27 22:52:11 +0300

    1534 Count Good Triplets

--- generate_solution_folder.py
+++ generate_solution_folder.py
@@ -18,32 +18,25 @@ def create_leetcode_problem_folder(problem_number, title, solution_code):
     with open(os.path.join(base_path, "README.md"), "w", encoding="utf-8") as f:
         f.write(f"# Leetcode {problem_number} - {title}\n\n")
         f.write(f"[🔗 Problem Link](https://leetcode.com/problems/{slugify(title)}/)\n\n")
-        f.write("## Description\n\n*Time Complexity: O(n))\nSpace Complexity: O(1)*\n\n")
+        f.write("## Description\n\n*Time Complexity: O(n^3))\nSpace Complexity: O(1)*\n\n")
         f.write("## Solution\n\nSee [`solution.py`](solution.py)\n")
 
     print(f"[✅] Created folder and files at: {base_path}")
 
 if __name__ == "__main__":
-    problem_number = "1071"
-    problem_title = " Greatest Common Divisor of string"
+    problem_number = "1534"
+    problem_title = "Count Good Triplets"
     solution_code = '''
 
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        n=0
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                for k in range(j+1, len(arr)):
+                    if abs(arr[i]-arr[j])<=a and abs(arr[j]-arr[k])<=b and abs(arr[i]-arr[k])<=c:
+                        n+=1
+        return n
 
-        if str1 + str2 != str2 + str1:
-            return ""
-        a=len(str1)
-        b=len(str2)
-        while b>0:
-            a,b=b, a%b
-        return str1[:a]
-# or
-        if str1 + str2 != str2 + str1:
-            return ""
-        gcd_len = math.gcd(len(str1), len(str2))
-        
-        return str1[:gcd_len]
-        
 '''
     create_leetcode_problem_folder(problem_number, problem_title, solution_code)
---
+++ leetcode-solutions/1534-count-good-triplets/README.md
@@ -0,0 +1,12 @@
+# Leetcode 1534 - Count Good Triplets
+
+[🔗 Problem Link](https://leetcode.com/problems/count-good-triplets/)
+
+## Description
+
+*Time Complexity: O(n^3))
+Space Complexity: O(1)*
+
+## Solution
+
+See [`solution.py`](solution.py)
