1d3197702863eae056839711b4257c3a919df6ac
Daniel <ethiomiracle2017@gmail.com>
2025-10-31 22:40:38 +0300

    293 Flip Game

---
+++ leetcode-solutions/ pro 293-flip-game/solution.py
@@ -0,0 +1,23 @@
+# Leetcode  pro 293:  Flip Game
+# https://leetcode.com/problems/flip-game/
+
+class Solution:
+    def generatePossibleNextMoves(self, currentState: str ) -> list[str]:
+        if len(currentState)==1:
+            return []
+        result=[]
+        for i in range(len(currentState)-1):
+            if currentState[i:i+2]=="++":
+                result.append(currentState[:i]+"--"+currentState[i+2:])
+        return result
+      # or
+      class Solution:
+def generatePossibleNextMoves(self, currentState: str) -> List[str]:
+        s = list(currentState)
+        result = []
+        for i, c in enumerate(s[:-1]):
+            if c == "+" and s[i + 1] == "+":
+                s[i] = s[i + 1] = "-"
+                result.append("".join(s))
+                s[i] = s[i + 1] = "+"
+        return result
