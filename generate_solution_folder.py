7d386da05223a21734087368faa080054e8f0eb9
Daniel <ethiomiracle2017@gmail.com>
2025-11-30 10:03:15 +0300

    pro 3631 -  Sort Threats by Severity and Exploitability

--- generate_solution_folder.py
+++ generate_solution_folder.py
@@ -31,57 +31,90 @@ def create_leetcode_problem_folder(problem_number, title, solution_code, descrip
     print(f"[✅] Created folder and files at: {base_path}")
 
 if __name__ == "__main__":
-    problem_number = "pro 2229"
-    problem_title = " Check if an Array Is Consecutive"
+    problem_number = "pro 3631"
+    problem_title = " Sort Threats by Severity and Exploitability"
     solution_code = '''class Solution:  
 """
 class Solution:
-    def isConsecutive(self, nums: List[int]) -> bool:
-        mi, mx = min(nums), max(nums)
-        n = len(nums)
-        return len(set(nums)) == n and mx == mi + n - 1
-#or 
+    def sortThreats(self, threats: List[List[int]]) -> List[List[int]]:
+        return sorted(threats,key=lambda x:(-(2 * x[1] + x[2]), x[0]))
+# or 
 class Solution:
-    def isConsecutive(self, nums: List[int]) -> bool:
-        x=len(nums)
-        n=min(nums)
-        for i in range(n, n + x):
-            if i not in set(nums):
-                return False
-        return True
+    def sortThreats(self, threats: List[List[int]]) -> List[List[int]]:
+        score=[0]*len(threats)
+        for i in range(len(threats)):
+            score[i] = 2 * threats[i][1] + threats[i][2]
+            for j in range(i+1, len(threats)):
+                score[j] = 2 * threats[j][1] + threats[j][2]
+                if score[i]<score[j]:
+                    threats[i], threats[j]=threats[j], threats[i]
+                elif score[i]==score[j] and threats[i][0]>threats[j][0]:
+                    threats[i], threats[j]=threats[j], threats[i] 
+        return threats
+            
+# or
+class Solution:
+    def sortThreats(self, threats: List[List[int]]) -> List[List[int]]:
+        threats.sort(key=lambda x:(-(2 * x[1] + x[2]), x[0]))
+        return threats
 """
 '''
-    description_text = """ You are given a string array features where features[i] is a single word that represents the name of a feature of the latest product you are working on. You have made a survey where users have reported which features they like. You are given a string array responses, where each responses[i] is a string containing space-separated words.
+    description_text = """ 
+You are given a 2D integer array threats, where each threats[i] = [IDi, sevi​, expi]
 
-The popularity of a feature is the number of responses[i] that contain the feature. You want to sort the features in non-increasing order by their popularity. If two features have the same popularity, order them by their original index in features. Notice that one response could contain the same feature multiple times; this feature is only counted once in its popularity.
+IDi: Unique identifier of the threat.
+sevi: Indicates the severity of the threat.
+expi: Indicates the exploitability of the threat.
+The score of a threat i is defined as: score = 2 × sevi + expi
 
-Return the features in sorted order.
+Your task is to return threats sorted in descending order of score.
+
+
+If multiple threats have the same score, sort them by ascending ID​​​​​​​.
 
  
 
 Example 1:
 
-Input: features = ["cooler","lock","touch"], responses = ["i like cooler cooler","lock touch cool","locker like touch"]
-Output: ["touch","cooler","lock"]
-Explanation: appearances("cooler") = 1, appearances("lock") = 1, appearances("touch") = 2. Since "cooler" and "lock" both had 1 appearance, "cooler" comes first because "cooler" came first in the features array.
+Input: threats = [[101,2,3],[102,3,2],[103,3,3]]
+
+Output: [[103,3,3],[102,3,2],[101,2,3]]
+
+Explanation:
+
+Threat	ID	sev	exp	Score = 2 × sev + exp
+threats[0]	101	2	3	2 × 2 + 3 = 7
+threats[1]	102	3	2	2 × 3 + 2 = 8
+threats[2]	103	3	3	2 × 3 + 3 = 9
+Sorted Order: [[103, 3, 3], [102, 3, 2], [101, 2, 3]]
 
 Example 2:
 
-Input: features = ["a","aa","b","c"], responses = ["a","a aa","a a a a a","b a"]
-Output: ["a","aa","b","c"]
+Input: threats = [[101,4,1],[103,1,5],[102,1,5]]
+
+Output: [[101,4,1],[102,1,5],[103,1,5]]
+
+Explanation:​​​​​​​
+
+Threat	ID	sev	exp	Score = 2 × sev + exp
+threats[0]	101	4	1	2 × 4 + 1 = 9
+threats[1]	103	1	5	2 × 1 + 5 = 7
+threats[2]	102	1	5	2 × 1 + 5 = 7
+threats[1] and threats[2] have same score, thus sort them by ascending ID.
+
+Sorted Order: [[101, 4, 1], [102, 1, 5], [103, 1, 5]]
+
  
+
 
 Constraints:
 
-1 <= features.length <= 104
-1 <= features[i].length <= 10
-features contains no duplicates.
-features[i] consists of lowercase letters.
-1 <= responses.length <= 102
-1 <= responses[i].length <= 103
-responses[i] consists of lowercase letters and spaces.
-responses[i] contains no two consecutive spaces.
-responses[i] has no leading or trailing spaces.
+1 <= threats.length <= 105
+threats[i] == [IDi, sevi, expi]
+1 <= IDi <= 106
+1 <= sevi <= 109
+1 <= expi <= 109
+All IDi are unique
 """
     
     create_leetcode_problem_folder(problem_number, problem_title, solution_code, description_text)
